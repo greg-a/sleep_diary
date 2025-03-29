1d31602bb36b7e1d298ba71c48705782bc4db8f0
greg allebach <greg.allebach@gmail.com>
2025-03-29 11:25:05 -0400

    adjust start and end selection to simplify ux

--- src/pages/diary/Diary.tsx
+++ src/pages/diary/Diary.tsx
@@ -28,10 +28,36 @@ function formatDate(d: Date) {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
-  const hour = String(date.getHours()).padStart(2, "0");
-  const minute = String(date.getMinutes()).padStart(2, "0");
-  return `${year}-${month}-${day}T${hour}:${minute}:00`;
+  return `${year}-${month}-${day}`;
 }
+
+function formatTime(d: Date) {
+  const hour = String(d.getHours()).padStart(2, "0");
+  const minute = String(d.getMinutes()).padStart(2, "0");
+  return `${hour}:${minute}`;
+}
+
+const getDuration = (start: Date, end: Date | undefined) => {
+  const durationStr = [];
+  if (!end) {
+    return "";
+  }
+  const startMs = start.getTime();
+  const endMs = end.getTime();
+  const duration = endMs - startMs;
+  const seconds = duration / 1000;
+  let minutes = seconds / 60;
+  const hours = Math.floor(minutes / 60);
+
+  if (hours > 0) {
+    durationStr.push(`${hours} hours`);
+  }
+  minutes = Math.floor(minutes % 60);
+  if (minutes > 0) {
+    durationStr.push(`${minutes} minutes`);
+  }
+  return durationStr.join(" ");
+};
 
 export const Diary = () => {
   const [entries, setEntries] = useState<Entry[]>([]);
@@ -57,15 +83,21 @@ export const Diary = () => {
   };
 
   const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const key = e.target.id as "startTime" | "endTime";
+    const newTime = new Date(newEntry[key] ?? newEntry.startTime);
+    const [hours, minutes] = e.target.value.split(":");
+    newTime.setHours(Number(hours));
+    newTime.setMinutes(Number(minutes));
+
     setNewEntry((prev) => ({
       ...prev,
-      [e.target.id]: new Date(e.target.value),
+      [key]: newTime,
     }));
   };
 
   const handleDeleteEntry = (entryId: string) => {
-    setEntries(prev => prev.filter(e => e.id !== entryId))
-  }
+    setEntries((prev) => prev.filter((e) => e.id !== entryId));
+  };
 
   useEffect(() => {
     (async () => {
@@ -73,9 +105,17 @@ export const Diary = () => {
       setEntries(entries);
     })();
   }, []);
+
   return (
     <>
       <h1>Sleep Diary</h1>
+      <div style={{ width: "fit-content" }}>
+        <Input
+          label="Date"
+          type="date"
+          value={formatDate(newEntry.startTime)}
+        />
+      </div>
       <div className="form-container">
         <Input
           label="Description"
@@ -87,16 +127,21 @@ export const Diary = () => {
         <Input
           label="Start Time"
           name="startTime"
-          type="datetime-local"
-          value={formatDate(newEntry.startTime)}
+          type="time"
+          value={formatTime(newEntry.startTime)}
           onChange={handleDateChange}
         />
         <Input
           label="End Time"
           name="endTime"
-          type="datetime-local"
-          value={formatDate(newEntry.endTime ?? newEntry.startTime)}
+          type="time"
+          value={formatTime(newEntry.endTime ?? newEntry.startTime)}
           onChange={handleDateChange}
+        />
+        <Input
+          label="Duration"
+          readOnly
+          value={getDuration(newEntry.startTime, newEntry.endTime)}
         />
       </div>
       <EntryList entries={entries} onDelete={handleDeleteEntry} />
